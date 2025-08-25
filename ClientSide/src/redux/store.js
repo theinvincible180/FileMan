6b16d26015a35714fb321ed1427d744ed54f3fd0
Aniket <pandeyaniket701@gmail.com>
2025-08-25 12:43:31 +0530

    Built redux store

--- ClientSide/src/redux/store.js
+++ ClientSide/src/redux/store.js
@@ -1,1 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import fileReducer from './slice/file/fileSlice.js';
+import authReducer from './slice/auth/authSlice.js';
+
+const store = configureStore({
+  reducer: {
+    file: fileReducer,
+    auth: authReducer,
+  },
+  devTools: true
+});
+
+export default store;
